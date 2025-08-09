b692450032d78882de43052957a6fe2ad39dbb07
niiofficial <rtackie007@st.ug.edu.gh>
2025-08-09 10:00:55 +0000

    finished with task 2

---
+++ dcit318-assignment2-11078425/Task2_Abstract/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Task2_Abstract
+{
+    abstract class Shape
+    {
+        public abstract double GetArea();
+    }
+
+    class Circle(double radius) : Shape
+    {
+        public double Radius { get; set; } = radius;
+
+        public override double GetArea()
+        {
+            return Math.PI * Radius * Radius;
+        }
+    }
+
+    class Rectangle(double length, double width) : Shape
+    {
+        public double Length { get; set; } = length;
+        public double Width { get; set; } = width;
+
+        public override double GetArea()
+        {
+            return Length * Width;
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            Circle circle = new(5);
+            Rectangle rectangle = new(4, 6);
+
+            Console.WriteLine($"Circle Area: {circle.GetArea()}");
+            Console.WriteLine($"Rectangle Area: {rectangle.GetArea()}");
+        }
+    }
+}
