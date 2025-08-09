e6924a432a4e3eb450a2ee5be2beb2ed0ae8b617
niiofficial <rtackie007@st.ug.edu.gh>
2025-08-09 10:01:15 +0000

    Finished with task 3

---
+++ dcit318-assignment2-11078425/Task3_Interface/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Task3_Interface
+{
+    interface IMovable
+    {
+        void Move();
+    }
+
+    class Car : IMovable
+    {
+        public void Move() => Console.WriteLine("Car is moving");
+    }
+
+    class Bicycle : IMovable
+    {
+        public void Move() => Console.WriteLine("Bicycle is moving");
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            Car car = new();
+            Bicycle bicycle = new();
+
+            car.Move();
+            bicycle.Move();
+        }
+    }
+}
