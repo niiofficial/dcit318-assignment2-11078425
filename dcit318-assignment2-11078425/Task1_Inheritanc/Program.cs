6228762810b3650e0928cfc05197913563136a4e
niiofficial <rtackie007@st.ug.edu.gh>
2025-08-09 10:00:26 +0000

    Finished with task 1

---
+++ dcit318-assignment2-11078425/Task1_Inheritanc/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Task1_Inheritance
+{
+    class Animal
+    {
+        public virtual void MakeSound() => Console.WriteLine("Some generic sound");
+    }
+
+    class Dog : Animal
+    {
+        public override void MakeSound() => Console.WriteLine("Bark");
+    }
+
+    class Cat : Animal
+    {
+        public override void MakeSound() => Console.WriteLine("Meow");
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            Animal genericAnimal = new();
+            Dog dog = new();
+            Cat cat = new();
+
+            genericAnimal.MakeSound();
+            dog.MakeSound();
+            cat.MakeSound();
+        }
+    }
+}
